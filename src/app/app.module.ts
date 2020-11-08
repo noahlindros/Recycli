9c3958786d34cc8881fe87fc045788ed690c2622
noahlindros <74145835+noahlindros@users.noreply.github.com>
2020-11-08 16:24:06 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { SectionsModule } from './sections/sections.module';
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+import { PresentationModule } from './presentation/presentation.module';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AboutComponent } from './pages/about/about.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        DashboardComponent,
+        RegisterComponent,
+        AboutComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        NgbModule,
+        FormsModule,
+        RouterModule,
+        AppRoutingModule,
+        PresentationModule,
+        SectionsModule,
+        ComponentsModule,
+        ExamplesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
